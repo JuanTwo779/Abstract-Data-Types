8ce05007f3bc264d9dd51f5ed8b6e166549709ff
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-08-03 14:05:21 +1000

    queue implementation

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue q = new Queue();
+        q.enQueue(7);
+        q.enQueue(19);
+        q.enQueue(1);
+        q.enQueue(13);
+
+        q.deQueue();
+        q.deQueue();
+        q.deQueue();
+
+        q.enQueue(109);
+        q.enQueue(100);
+        q.enQueue(130);
+        q.enQueue(145);
+
+        System.out.println(q.isEmpty());
+
+        System.out.println(q.size());
+
+        //q.show();
+
+
     }
 }
--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,2 +1,72 @@
-public class Queue {
+public class Queue
+{
+    int queue[] = new int[5];
+    int size;
+    int front; //left
+    int rear; //right, if we have values
+
+    public void enQueue(int data) //push
+    {
+        if(!isFull())
+        {
+            queue[rear] = data;
+            rear = (rear + 1) % 5;
+            size++;
+        }
+        else
+            System.out.println("Queue is full");
+    }
+
+    public int deQueue() //pop
+    {
+        int data = queue[front];
+        if(!isEmpty())
+        {
+            front = (front + 1) % 5;
+            size--;
+        }
+        else
+            System.out.println("Queue is empty");
+
+
+        return data;
+    }
+
+    public void first() //peek
+    {
+        System.out.println("The element in the front of the queue is: ");
+        int data = queue[front];
+        System.out.println(data);
+    }
+
+    public int size()
+    {
+
+        return this.size;
+    }
+
+    public boolean isEmpty()
+    {
+        return size==0;
+    }
+
+    public boolean isFull()
+    {
+        return size()==5;
+    }
+
+    public void show()
+    {
+        System.out.println("Elements: ");
+        for (int i = 0; i < size; i++)
+        {
+            System.out.print(queue[(front + i) % 5] + " ");
+        }
+        System.out.println(" ");
+
+        for (int n: queue)
+        {
+            System.out.print(n + " ");
+        }
+    }
 }
